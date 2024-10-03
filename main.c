e734d64e9b12a4db1517bf2e1fcdc4e005147a86
Margaux REVELLE MONTEIRO <margaux.revelle@esme.fr>
2024-10-03 16:35:05 +0200

    Ajout de int r = move_forward() ; dans main.c

--- main.c
+++ main.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    printf("Bonjour le monde");
-    return 0;
-}
-
-// Voici une nouvelle ligne
-//Voici une troisième modification de main.c
-#include <stdio.h>
-
-int main() {
-    printf(Bonjour la classe);
-    return 0;
-}
+int r = move_forward() ;
